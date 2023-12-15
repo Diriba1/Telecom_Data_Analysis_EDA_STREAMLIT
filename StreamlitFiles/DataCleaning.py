c9ddf73a2cbc0c4b56235e4406f755239556cb1f
Diriba <39425889+Diriba1@users.noreply.github.com>
2023-12-15 17:40:43 +0300

    finishing on data cleaning part of streamlit

--- StreamlitFiles/DataCleaning.py
+++ StreamlitFiles/DataCleaning.py
@@ -15,9 +15,15 @@ def app():
     
     #print concise summarry of data frame
     st.markdown("#### Print concise summary of a dataframe")
-    st.write(df.shape)
-    st.write(df.dtypes)
-    st.write("the data has 55 columns and 150001 rows with 50 of the columns data types are float and the rest 5 are object data type.")
+
+    col1, col2 = st.columns([1, 3])
+    with col1:   
+        st.write(df.dtypes)
+        st.write(df.shape)
+    with col2: 
+        st.write(df.describe())
+
+    st.write("The data has 55 columns and 150001 rows with 50 of the columns data types are float and the rest 5 are object data type.")
 
 
     # data cleaning
@@ -30,19 +36,30 @@ def app():
         st.write(missing_df)
     with col2:
         st.markdown("#### Columns to be dropped")
-        st.markdown("identifying and dropping the columns, with missing values greater than 25%")
+        st.markdown("Identifying and dropping the columns, with missing values greater than 25%")
         columns_to_drop = list(missing_df[missing_df["Missing Percentage"]>=25].Column.values)
         df = df.drop(columns_to_drop, axis=1)
         st.write(columns_to_drop)
-        st.write("our data shape after dropping missing columns", df.shape)
-   
-    st.write("In addition to dropping missing values of 25%, we are going to drop the raw because it is total row, and change the data type of 'IMEI', 'IMSI', 'MSISDN/Number' to string data type. ")
+        st.write("In addition to dropping missing values of 25%, we are have to drop the raw because it is a total row, and change the data type of 'IMEI', 'IMSI', 'MSISDN/Number' to string data type. ")
+        st.write("Then identify and populate float data type columns with mean values and string data types with mode value")
+    
     # List of columns to convert to string
     columns_to_convert = ['IMEI', 'IMSI', 'MSISDN/Number']
 
     # Convert selected columns to string data type
     df[columns_to_convert] = df[columns_to_convert].astype(str)
-    st.write(df.dtypes)
+    # Drop the last row 
+    df.drop(df.index[-1], inplace=True)
+
+    # Identify float-type columns
+    float_columns = df.select_dtypes(include='float').columns
+
+    # Populate float-type columns with mean value
+    df[float_columns] = df[float_columns].fillna(df[float_columns].mean())
+    st.write("The data shape is now: ", df.shape)
+    
+    st.write(df)
+    st.write("#### Saving the cleaned data to CSV file for easy access and further processes")
 
     
 
